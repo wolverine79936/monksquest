aa56152056aae7c6a17b07a3052dbf1ea7427cc0
wolverine79936 <abbotyuri@gmail.com>
2021-11-21 22:32:35 -0700

    Final commit for the day: We now have a window with a black background that hangs around and closes automatically after 10000 ms.

---
+++ Makefile
@@ -0,0 +1,9 @@
+#OBJS specifies which files to compile as part of the
+#monksquest project.
+OBJS = src/main.cpp
+
+#OBJ_NAME specifies the name of our final executable
+OBJ_NAME = monksquest
+#This is the target that compiles our executable
+all: $(OBJS)
+				g++ $(OBJS) -Werror -pedantic -lSDL2 -o bin/$(OBJ_NAME)
--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,36 @@ int main(int argc, char* args[])
     // Let's initialize SDL
     if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
     {
+        // This checks that we succesfully created our window:
+        g_pWindow = SDL_CreateWindow("Monk's Quest", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 
+        // If we succesfully created our renderer within the window:
+        if(g_pWindow != 0)
+        {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
     }
     else
     {
-        
+        return 1; // For some reason, we couldn't initialize SDL.
     }
+
+    // Everything has succeeded, so let draw the window finally.
+
+    // We'll set the renderer background to black. // This function expects RGBa as color values.
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+    // Now let's clear the window to black.
+    SDL_RenderClear(g_pRenderer);
+
+    // And now to show the window.
+    SDL_RenderPresent(g_pRenderer);
+
+    // And now to set a delay before quitting
+    SDL_Delay(10000);
+
+    // Finally, to clean up SDL
+    SDL_Quit();
+
     return 0;
 }
